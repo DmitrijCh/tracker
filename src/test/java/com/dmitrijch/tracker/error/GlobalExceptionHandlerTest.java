cf354090372095bf4b372b072fd66f8809761de9
dmitrij <dmitrij.chvanov@gmail.com>
2023-09-20 17:12:41 +0400

    Deleted error test comment

--- src/test/java/com/dmitrijch/tracker/error/GlobalExceptionHandlerTest.java
+++ src/test/java/com/dmitrijch/tracker/error/GlobalExceptionHandlerTest.java
@@ -26,27 +26,21 @@ public class GlobalExceptionHandlerTest {
 
     @Test
     public void testHandleValidationException() {
-        // Создаем объект MethodArgumentNotValidException и BindingResult для тестирования
         MethodArgumentNotValidException ex = mock(MethodArgumentNotValidException.class);
         BindingResult bindingResult = mock(BindingResult.class);
 
-        // Создаем список FieldError для BindingResult
         List<FieldError> fieldErrors = List.of(
                 new FieldError("field1", "field1", "Error 1"),
                 new FieldError("field2", "field2", "Error 2")
         );
 
-        // Устанавливаем поведение mock объектов
         when(ex.getBindingResult()).thenReturn(bindingResult);
         when(bindingResult.getFieldErrors()).thenReturn(fieldErrors);
 
-        // Вызываем обработчик и получаем результат
         ResponseEntity<?> responseEntity = globalExceptionHandler.handleValidationException(ex);
 
-        // Проверяем, что статус ответа соответствует ожидаемому (HttpStatus.BAD_REQUEST)
         assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
 
-        // Проверяем, что ответ содержит ожидаемые ошибки в виде Map
         Map<String, String> responseBody = (Map<String, String>) responseEntity.getBody();
         assertEquals(2, responseBody.size());
         assertEquals("Error 1", responseBody.get("field1"));
